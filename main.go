282c28f24cfc4678edea343a7b814c3fcfc6eeb4
david <dbunniadyd@yahoo.com>
2022-08-22 20:52:08 +0700

    third assigment

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
+	listFriend()
 
+}
+
+func listFriend() {
 	name := []string{"david", "fadli", "fisa", "rafi", "kadek", "kevin", "iqbal", "irfan", "edwin", "jaka"}
 
 	for i, t := range name {
