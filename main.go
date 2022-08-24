17a33d65b7d4964e5bc1dffd9732441395c434fc
david <dbunniadyd@yahoo.com>
2022-08-24 20:36:37 +0700

    done: ass-1

--- main.go
+++ main.go
@@ -1,15 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Person struct {
+	name    string
+	address string
+	job     string
+	reason  string
+}
 
 func main() {
-	for i := 0; i <= 10; i++ {
-		fmt.Print(i)
-		if i%2 == 0 {
-			fmt.Println(" Genap")
-		} else {
-			fmt.Println(" Ganjil")
-		}
+
+	poeple := []Person{
+		{
+			name:    "david",
+			address: "Tanjung Duren",
+			job:     "Bos Pertamina",
+			reason:  "iseng",
+		},
+		{
+			name:    "fadli",
+			address: "Cempaka Putih",
+			job:     "Dirut Pertamina",
+			reason:  "iseng juga",
+		},
 	}
 
+	args := os.Args
+	indexArgs := args[1]
+	index, _ := strconv.Atoi(indexArgs)
+
+	for i, t := range poeple {
+		if i == index-1 {
+			fmt.Println(t)
+		}
+	}
 }
